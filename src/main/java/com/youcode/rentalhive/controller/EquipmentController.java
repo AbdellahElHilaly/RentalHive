876c93f27fad302919a8b6b2f71e68423eb36a56
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-20 19:27:05 +0100

    Filter | Add the getAllSearchedEquipments method to get the equipments getMapping response return - controller/EquipmentController

--- src/main/java/com/youcode/rentalhive/controller/EquipmentController.java
+++ src/main/java/com/youcode/rentalhive/controller/EquipmentController.java
@@ -2,68 +2,72 @@ package com.youcode.rentalhive.controller;
 
 import com.youcode.rentalhive.dao.model.Equipment;
 import com.youcode.rentalhive.dao.service.EquipmentService;
+import com.youcode.rentalhive.dao.service.impl.EquipmentServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@AllArgsConstructor
+@RequestMapping("/api")
 public class EquipmentController {
-    private final EquipmentService equipmentService;
+    @Autowired
+    private EquipmentServiceImpl equipmentServiceImpl;
 
-//    @Autowired
-//    public EquipmentController(EquipmentService equipmentService) {
-//        this.equipmentService = equipmentService;
-//    }
+    @GetMapping("/equipments")
+    public ResponseEntity<Map<String, Object>> getAllSearchedEquipments(@RequestParam String search) {
+        Map<String,Object> response = new HashMap<>();
+        List<Equipment> equipments = equipmentServiceImpl.searchedEquipments(search);
+
+        if (equipments.isEmpty()) {
+            System.out.println("jksdjk");
+            response.put("status", "error");
+            response.put("message", "search is empty");
+
+            return ResponseEntity.badRequest().body(response);
+       }
+        response.put("status", "sucess");
+        response.put("equipments", equipments);
+
+        return ResponseEntity.ok(response);
+    }
+
+   @Autowired
+    public EquipmentController(EquipmentService equipmentService) {
+        this.equipmentService = equipmentService;
+    }
 
 
     @GetMapping("/equipments")
     public ResponseEntity<List<Equipment>> getAllEquipments() {
-
         return ResponseEntity.ok(equipmentService.selectAll());
-
     }
 
     @PostMapping("/addEquipment")
-    public ResponseEntity<Equipment> add(){
-        try{
+    public ResponseEntity<Equipment> add() {
+     try {
+         Equipment equipment = Equipment.builder()
+                 .name("trax")
+                 .id(1L)
+                 .Quantity(44)
+                 .build();
+         Equipment savedEquipment = equipmentService.addEquipment(equipment);
 
-            Equipment equipment = Equipment.builder()
-                    .name("trax")
-                    .id(1L)
-                    .Quantity(44)
-                    .build();
+         return ResponseEntity.ok(savedEquipment);
 
-            Equipment savedEquipment = equipmentService.addEquipment(equipment);
+     }catch (Exception e) {
+         System.out.println(e);
 
-            return ResponseEntity.ok(savedEquipment);
-
-        }catch(Exception e){
-            System.out.println(e);
-            return null;
-        }
-
-
-
-
-
-//        Widget testWidget = Widget.builder()
-//                .name("foo")
-//                .id(1)
-//                .build();
-
-
-
-
+         return null;
+     }
+       Widget testWidget = Widget.builder()
+               .name("foo")
+               .id(1)
+               .build();
     }
-
-
-
-
 }
