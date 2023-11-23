60c580ef7a41c851c6fa710122067b4a691178c3
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-11-23 09:32:16 +0100

    fix errors

--- src/main/java/com/youcode/rentalhive/helper/Authentication.java
+++ src/main/java/com/youcode/rentalhive/helper/Authentication.java
@@ -3,12 +3,10 @@ package com.youcode.rentalhive.helper;
 import com.youcode.rentalhive.dao.model.User;
 import lombok.AllArgsConstructor;
 
-import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @AllArgsConstructor
 public class Authentication {
-
 
     public Optional<User> authenticate(User user, String password) {
         if (user.getPassword().equals(password)) {
@@ -17,7 +15,6 @@ public class Authentication {
         }
         return Optional.empty();
     }
-
 
 
 }
